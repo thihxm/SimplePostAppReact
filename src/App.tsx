d12bcc509c79171c5b589ceefd312dfe953b65fe
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-06-25 23:11:01 -0300

    Default styling + sidebar component

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Header } from './components/Header'
+import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          Post
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
