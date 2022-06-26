a2ccc00aa7bed9396793475e6e90bcf6325984f9
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-06-26 00:24:22 -0300

    Comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/thihxm.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Thiago Medeiros</strong>
+              <time title='25 de julho de 2022 às 22:00h' dateTime='2022-06-25 22:00:00'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>10</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
